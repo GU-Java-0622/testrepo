c66747eff135c21e8d7e86a4209c081292e43ded
madlur <2185430@gmail.com>
2022-06-23 16:35:33 +0300

    загрузил каркас

---
+++ src/main/java/ru/geekbrains/testrepo/TestrepoApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.testrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrepoApplication.class, args);
+	}
+
+}
