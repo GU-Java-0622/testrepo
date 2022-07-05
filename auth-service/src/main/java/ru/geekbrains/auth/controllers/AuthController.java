911686159e16eb2dacfb6142d8e9d5ad4ed9c693
madlur <2185430@gmail.com>
2022-07-05 14:06:58 +0300

    PRJ-2. fix liquibase script + added swagger

--- auth-service/src/main/java/ru/geekbrains/auth/controllers/AuthController.java
+++ auth-service/src/main/java/ru/geekbrains/auth/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package ru.geekbrains.auth.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +19,19 @@ import javax.security.auth.message.AuthException;
 @RestController
 @RequestMapping("api/auth")
 @RequiredArgsConstructor
+@Tag(name = "Сервис авторизации и аутентификации", description = "Методы работы с сервисом авторизации и аутентификации")
 public class AuthController {
 
     private final AuthService authService;
+
+    @Operation(
+            summary = "Запрос на авторизацию пользователя",
+            responses = {
+                    @ApiResponse(
+                            description = "Пользователь авторизован", responseCode = "200"
+                    )
+            }
+    )
 
     @PostMapping("login")
     public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest authRequest) throws AuthException {
@@ -26,11 +39,29 @@ public class AuthController {
         return ResponseEntity.ok(token);
     }
 
+    @Operation(
+            summary = "Запрос на получение auth-токена",
+            responses = {
+                    @ApiResponse(
+                            description = "Токен получен", responseCode = "200"
+                    )
+            }
+    )
+
     @PostMapping("token")
     public ResponseEntity<JwtResponse> getNewAccessToken(@RequestBody RefreshJwtRequest request) throws AuthException {
         final JwtResponse token = authService.getAccessToken(request.getRefreshToken());
         return ResponseEntity.ok(token);
     }
+
+    @Operation(
+            summary = "Запрос на получение auth-токена и refresh-токена",
+            responses = {
+                    @ApiResponse(
+                            description = "Токены получены", responseCode = "200"
+                    )
+            }
+    )
 
     @PostMapping("refresh")
     public ResponseEntity<JwtResponse> getNewRefreshToken(@RequestBody RefreshJwtRequest request) throws AuthException {
