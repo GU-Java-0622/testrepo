424288faed3308a005e44ba1e7fe645318362a45
madlur <59394178+madlur@users.noreply.github.com>
2022-07-06 23:26:04 +0300

    Изменен код auth-service

---
+++ auth-service/src/main/java/ru/geekbrains/auth/security/jwt/JwtUtils.java
@@ -0,0 +1,73 @@
+package ru.geekbrains.auth.security.jwt;
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import ru.geekbrains.auth.security.services.UserDetailsImpl;
+
+import java.util.Date;
+
+@Component
+public final class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${jwt.secret.access}")
+    private String jwtSecret;
+
+    @Value("${jwt.access.expiration}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getEmail()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
+
+//    public static JwtAuthentication generate(Claims claims) {
+//        final JwtAuthentication jwtInfoToken = new JwtAuthentication();
+////        jwtInfoToken.setRoles(getRoles(claims));
+//        jwtInfoToken.setFirstName(claims.get("firstName", String.class));
+//        jwtInfoToken.setUsername(claims.getSubject());
+//        return jwtInfoToken;
+//    }
+
+//    private static Set<String> getRoles(Claims claims) {
+//        final List<String> roles = claims.get("roles", List.class);
+//        return roles.stream()
+//                .collect(Collectors.toSet());
+//    }
+
+}
